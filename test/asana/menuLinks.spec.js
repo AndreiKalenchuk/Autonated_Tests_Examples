e35b3a95189ef198bbc4402314c5d07977e9bd35
Amdrei Kalianchuk <andrey.kalenchuk@gmail.com>
2020-06-18 16:05:26 -0400

    recorder refactor asana refactor

--- test/asana/menuLinks.spec.js
+++ test/asana/menuLinks.spec.js
@@ -1,44 +1,43 @@
-import { expect } from "chai";
+import {expect} from "chai";
 import HomePage from "./_page/HomePage";
 import {
-  homeURL,
-  homePageHeader,
-  homePageTitle,
-  menuLinksTextArr
+    homeURL,
+    homePageHeader,
+    homePageTitle,
+    menuLinksTextArr
 } from "./_data/homePageData";
 
 describe("HOME PAGE MENU LINKS", () => {
-  before("Go to asana verify url", () => {
-    browser.maximizeWindow();
-    HomePage.open(homeURL);
-    browser.waitUntil(
-      () => expect(browser.getTitle()).eq(homePageTitle),
-      10,
-      "Wrong title"
-    );
-  });
+    before("Go to asana verify url", () => {
+        browser.maximizeWindow();
+        HomePage.open(homeURL);
+        browser.waitUntil(
+            () => expect(browser.getTitle()).eq(homePageTitle),
+            10,
+            "Wrong title"
+        );
+    });
 
-  it("should verify menu links text", () => {
-    const el = HomePage.menuLinksList;
-    for (let i = 0; i < el.length; i++) {
-      expect(el[i].getText()).eq(menuLinksTextArr[i]);
-    }
-  });
+    it("should verify menu links text", () => {
+        const menuLinks = HomePage.menuLinksList;
+        for (let i = 0; i < menuLinks.length; i++) {
+            expect(menuLinks[i].getText()).eq(menuLinksTextArr[i]);
+        }
+    });
 
-  it("should verify menu links is Clickable", () => {
-    HomePage.menuLinksList.forEach(el => expect(el.isClickable()).true);
-  });
+    it("should verify menu links is Clickable", () => {
+        HomePage.menuLinksList.forEach(el => expect(el.isClickable()).true);
+    });
 
-  it("click on menu links should drop down sub menu links", () => {
-    const el = HomePage.menuLinksList;
-    for (let i = 0; i < el.length; i++) {
-      el[i].click();
-      browser.waitUntil(() => {
-        return expect(HomePage.subMenuActive.isEnabled()).true,
-          5000,
-          "No sub menu dropped down";
-      });
-      browser.refresh();
-    }
-  });
+    it("click on menu links should drop down sub menu links", () => {
+        const el = HomePage.menuLinksList;
+        for (let i = 0; i < el.length; i++) {
+            el[i].click();
+            browser.waitUntil(() => {
+                return expect(HomePage.subMenuActive.isDisplayed()).true,
+                    5000,
+                    "No sub menu dropped down";
+            });
+        }
+    });
 });
